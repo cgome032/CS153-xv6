f1038ec634070fca9a089d230d3b9c2b581890a9
Jeremy O'Neill <jonei005@ucr.edu>
2018-01-20 17:49:51 -0800

    started adding test cases to tests.c

--- tests.c
+++ tests.c
@@ -1,1 +1,19 @@
 // Test file
+
+int stdout = 1;
+
+void waittest(void) {
+	int pid;
+	
+	printf(stdout, "waittest started\n");
+	
+}
+
+void waitpidtest(void) {
+	int i, pid;
+	
+	printf(stdout, "waitpidtest started\n");
+	
+	
+	
+}
