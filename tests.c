13d61d15712a24824f831efe02ccc1a29d50a96b
Jeremy O'Neill <jonei005@ucr.edu>
2018-01-23 10:16:31 -0800

    adding test stubs to tests.c

--- tests.c
+++ tests.c
@@ -1,19 +1,35 @@
 // Test file
+#include "param.h"
+#include "types.h"
+#include "stat.h"
+#include "user.h" // printf()
+#include "fs.h"
+#include "fcntl.h"
+#include "syscall.h"
+#include "traps.h"
+#include "memlayout.h"
 
 int stdout = 1;
 
+void exittest() {
+	printf(stdout, "exittest started\n");
+}
+
 void waittest(void) {
-	int pid;
+	//int pid;
 	
 	printf(stdout, "waittest started\n");
 	
 }
 
 void waitpidtest(void) {
-	int i, pid;
+	//int i, pid;
 	
 	printf(stdout, "waitpidtest started\n");
 	
-	
-	
+}
+
+int main() {
+	printf(stdout, "in tests.c main function\n");
+	return 0;
 }
